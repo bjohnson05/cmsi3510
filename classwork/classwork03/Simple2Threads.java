e4eced14c5f5d2f2de3102f5f226343d1ccd6f08
B.J. Johnson <cmsi.bj.lmu@gmail.com>
2022-01-31 12:50:48 -0800

    added gofish.c solution, updated simple2threads.java

--- classwork/classwork03/Simple2Threads.java
+++ classwork/classwork03/Simple2Threads.java
@@ -1,20 +1,31 @@
    public class Simple2Threads {
+
+      static int parentTime = 5000;
+      static int childTime  = 3000;
+
       public static void main( String[] args ) {
+
+         if( args.length == 1 ) {
+            parentTime = Integer.parseInt( args[0] ) * 1000;
+         } else if( args.length == 2 ) {
+            parentTime = Integer.parseInt( args[0] ) * 1000;
+            childTime  = Integer.parseInt( args[1] ) * 1000;
+         }
          System.out.println( "\n...Starting program Simple2threads [in java]..." );
-         System.out.println( "......Parent started, will start child then sleep 5000 milliseconds..." );
+         System.out.println( "......Parent started, will start child then sleep " + parentTime + " milliseconds..." );
 
          Thread childThread = new Thread( new Runnable() {
             public void run() {
-               System.out.println( "......Child started, sleeping 3000 milliseconds..." );
-               sleep(3000);
-               System.out.println( "......Child is done sleeping 3 seconds.\n" );
+               System.out.println( "......Child started, sleeping " + childTime + " milliseconds..." );
+               sleep( childTime );
+               System.out.println( "......Child is done sleeping " + childTime/1000 + " seconds.\n" );
             }
          });
 
          System.out.println( "\n......Starting child thread..." );
          childThread.start();
-         sleep(5000);
-         System.out.println("......Parent is done sleeping 5 seconds.\n\n");
+         sleep( parentTime );
+         System.out.println("......Parent is done sleeping " + parentTime/1000 + " seconds.\n\n");
 
       }
       private static void sleep( int milliseconds ) {
