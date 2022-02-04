6a242a268a152d892f9235aa41967024dd23f675
B.J. Johnson <cmsi.bj.lmu@gmail.com>
2022-02-04 15:19:55 -0800

    added code to machelp.c

--- classwork/classwork04/machelp.c
+++ classwork/classwork04/machelp.c
@@ -1,1 +1,38 @@
+// Mac version
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include <unistd.h>
+
+// This is the data structure we will use to pass arguments to the children
+struct MyData {
+   char threadLetter;
+   int  secondsToRun;
+};
+
+#define TIME_SLICE 40
+
+// This is the child thread function that will be started by "pthread_create()"
+static void *child( void *passedData ) {
+
+   struct MyData *childData = (struct MyData*)passedData;  // data struct passed in
+   char threadLetter = childData->threadLetter;            // remember the struct is a pointer
+   int  timeToLive   = childData->secondsToRun;            //   so we use the "->" to access fields
+
+   // Loop until the time to live expires, then exit
+   while( timeToLive >= 0 ) {
+      timeToLive -= TIME_SLICE;
+      if( timeToLive <= 0 ) {
+         if( (timeToLive + TIME_SLICE) != 0 ) {
+            printf( "%c:%d:done\n", threadLetter, timeToLive + TIME_SLICE );
+         }
+      } else {
+         printf( "%c:%d:%d\n", threadLetter, TIME_SLICE, timeToLive );
+      }
+      sleep( 1 );
+   }
+   return NULL;
+}
 
